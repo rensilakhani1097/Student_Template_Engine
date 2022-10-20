816f77e6ebd3b286e95f02a71493822cd0dbb7d3
RencyLakhani <108806465+RencyLakhani@users.noreply.github.com>
2022-10-20 23:13:22 +0530

    Done

---
+++ models/Register.js
@@ -0,0 +1,22 @@
+require('../config/db');
+const mongoose = require("mongoose");
+const registerSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String,
+        required:true
+    }  
+})
+const Register = new mongoose.model("Register",registerSchema)
+module.exports= Register;
+
+
+
+
+
